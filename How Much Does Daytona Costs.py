467ac9d8a1b7681a47f8a36ff9216003b3394cce
Hamza Hussein <hamzahussain5051@gmail.com>
2026-02-23 15:48:39 +0300

    Create How Much Does Daytona Costs.py

---
+++ How Much Does Daytona Costs.py
@@ -0,0 +1,10 @@
+#flenbr
+t = int(input())
+for i in range(t):
+    n , k = map(int,input().split())
+    a = list(map(int,input().split()))
+    if k in a :
+        print("YES")
+    else:
+        print("NO")
+    
